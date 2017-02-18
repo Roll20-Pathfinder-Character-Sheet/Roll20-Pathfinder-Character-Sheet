49c0ea2787ba1f338ae2bc1ac91d81fcd8f9dbf8
Aaron Aichlmayr <waterfoul@gmail.com>
2017-02-18 15:27:37 -0600

    Seperated some bits out

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ var webpackConfig = {
             loader: 'html-loader'
           }
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
       }
     ]
   },
