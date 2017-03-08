da6e4a5cb624ff5036b6b40b0f4c8c55b58e6e66
Chris Buchholz <cbuchholz@feedingamerica.org>
2017-03-08 00:53:21 -0500

    updating index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import _ from 'underscore';
-import {on,randomInteger,getsectionIDs,getTranslationByKey,getAttrs,setAttrs,removeRepeatingRow,generateRowID} from '../stubs/on';
 import TAS from 'exports-loader?TAS!TheAaronSheet';
 import {PFLog,PFConsole} from './PFLog';
-import * as SWUtils from './SWUtils';
-import PFConst from './PFConst';
-import PFDB from './PFDB';
-import * as PFUtils from './PFUtils';
-import * as PFUtilsAsync from './PFUtilsAsync';
-import * as PFMacros from './PFMacros';
-import * as PFMenus from './PFMenus';
-import * as PFMigrate from './PFMigrate';
-import * as PFAttackGrid from './PFAttackGrid';
-import * as PFAttackOptions from './PFAttackOptions';
-import * as PFDefense from './PFDefense';
-import * as PFEncumbrance from './PFEncumbrance';
-
-//how to run this before anything? even before stuff is loaded?
+import * as PFSheet from './PFSheet';
+//importing PFSheet imports everything else
 TAS.config({
  logging: {
    info: true,
