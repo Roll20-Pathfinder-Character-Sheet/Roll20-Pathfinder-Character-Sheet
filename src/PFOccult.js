0e3e97b0a4f7790c38e2ab9162b0f45c849b6930
plutosdad <chriswb71@gmail.com>
2017-07-14 16:11:32 -0400

    fix burn use total level hd

--- src/PFOccult.js
+++ src/PFOccult.js
@@ -8,6 +8,7 @@ import PFConst from './PFConst';
 on("change:use_burn",TAS.callback(function eventUseBurn(eventInfo){
     getAttrs(['use_burn'],function(v){
         if(parseInt(v.use_burn,10)){
+            //set initial values
             SWUtils.evaluateAndSetNumber('kineticistburn_macro_text','kineticistburn_max',0);
             SWUtils.evaluateAndSetNumber('internalbuffer_macro_text','internalbuffer_max',0);
         }
@@ -18,38 +19,29 @@ on("change:use_burn",TAS.callback(function eventUseBurn(eventInfo){
 on("change:kineticistburn",TAS.callback(function eventUpdateBurn(eventInfo){
     if(eventInfo.sourceType==="player"){
 		TAS.debug("caught " + eventInfo.sourceAttribute + " event: " + eventInfo.sourceType);
-        getAttrs(['non-lethal-damage','kineticistburn','oldkineticistburn','internalbuffer_macro_text',
-            'class-0-level','class-1-level','class-2-level','class-3-level','class-4-level',
-            'class-5-level','level','npc-hd-num'],function(v){
-            var nld=0,oldburn=0,newburn=0,diff=0,level=0,tempstr='',newdmg=0,newnld=0,matches,setter={};
+        getAttrs(['non-lethal-damage','kineticistburn','oldkineticistburn','level'],function(v){
+            var nld=0,oldburn=0,newburn=0,diff=0,level=0,newdmg=0,newnld=0,setter={};
             try{
-                nld=parseInt(v['non-lethal-damage'],10)||0;
                 oldburn=parseInt(v['oldkineticistburn'],10)||0;
                 newburn=parseInt(v['kineticistburn'],10)||0;
                 if(oldburn!==newburn){
                     setter.oldkineticistburn=newburn;
+                    nld=parseInt(v['non-lethal-damage'],10)||0;
                     diff = (newburn-oldburn)||0;
-                    if(v.internalbuffer_macro_text){
-                        matches=v.internalbuffer_macro_text.match(/class\-0\-level|class\-1\-level|class\-2\-level|class\-3\-level|class\-4\-level|class\-5\-level|level|npc\-hd\-num/i);
-                        if(matches ){
-                            //TAS.debug("the values are nonlethal:"+nld+", old:"+oldburn+", new:"+newburn+", diff:"+diff,v);
-                            tempstr=matches[0].toLowerCase();
-                            level = parseInt(v[tempstr],10)||0;
-                            newdmg=diff*level;
-                            //TAS.debug("the value of "+tempstr+" is "+ level+", newdmg:"+newdmg+", curr nld:"+nld);
-                            newnld = nld + newdmg;
-                            if (newnld<0){
-                                newnld=0;
-                            }
-                            setter['non-lethal-damage']=newnld;
-                        }
+                    level = parseInt(v.level,10)||0;
+                    newdmg=diff*level;
+                    TAS.debug("PFOccult.change:kineticistburn level:"+ level+", newdmg:"+newdmg+", curr nld:"+nld,v);
+                    newnld = nld + newdmg;
+                    if (newnld<0){
+                        newnld=0;
                     }
+                    setter['non-lethal-damage']=newnld;
                 }
             } catch (err){
-                TAS.error("PFOccult modifle kineticburn",err);
+                TAS.error("PFOccult.change:kineticistburn kineticburn",err);
             } finally {
                 if (_.size(setter)){
-                    SWUtils.setWrapper(setter);
+                    SWUtils.setWrapper(setter,PFConst.silentParams);
                 }
             }
 
