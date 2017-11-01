ec677556ea73b20f95ea603c20d8923800caaf65
plutosdad <chriswb71@gmail.com>
2017-11-01 10:58:14 -0400

    update version

--- src/PFConst.js
+++ src/PFConst.js
@@ -1,7 +1,7 @@
 export default {
 	/* Pathfinder SHEET constants */
-	version: 1.696,
-	announcementVersionAttr: 'attentionv169-show',
+	version: 1.697,
+	announcementVersionAttr: 'attentionv1697-show',
 
 	/***************************************Lists of Fields ************************************************************/
 	//add any new repeating sections here. This is the word after "repeating_"
