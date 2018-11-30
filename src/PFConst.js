c5195436ed632f7b807533eeb18f3d05f002c737
Vince <sefnerr@charter.net>
2018-11-29 23:42:28 -0800

    Prep for next PR

--- src/PFConst.js
+++ src/PFConst.js
@@ -1,6 +1,6 @@
 export default {
 	/* Pathfinder SHEET constants */
-	version: 1.729,
+	version: 1.73,
 	announcementVersionAttr: 'attentionv172-show',
 
 	/***************************************Lists of Fields ************************************************************/
