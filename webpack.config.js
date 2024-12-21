861c8cd278ddcb923d8b5edbce869fe0ff1cf876
∇ince <vince-roll20@users.noreply.github.com>
2024-12-21 07:43:12 -0800

    working as webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const mode = process.env.NODE_ENV;
-console.log(`~~~ webpack is in :${mode} mode ~~~`);
+const HtmlWorkerScriptPlugin = require('./HtmlWorkerScriptPlugin.js');
+// const mode = process.env.NODE_ENV;
 
 const webpackConfig = {
   entry: path.join(__dirname, 'src/index.js'),
@@ -33,39 +33,45 @@ const webpackConfig = {
       template: path.join(__dirname, 'src/index.html'),
       inlineSource: '.js$',
       minify: false,
+      // inject: 'body',
+      inject: false, // Disables automatic script injection
+      // scriptLoading: 'blocking',
     }),
     new HtmlInlineScriptPlugin(),
-    function HtmlWorkerScriptPlugin() {
-      this.plugin('emit', (compilation, callback) => {
-        const htmlAsset = compilation.assets['index.html'];
-        if (htmlAsset) {
-          let html = htmlAsset.source();
-          // replace <script> tag for roll20 sheetworkers
-          html = html.replace(/<script>(.*?)<\/script>/gs, '<script type="text/worker">$1</script>');
-          const currentDate = new Date().toUTCString();
-          html = html.replace('$$CURRENTRELEASEDATE$$', currentDate);
-          if (mode === 'production') {
-            html = html.replace(/^\s*<!--DEVELOPMENT CODE\. NOT READY FOR PRODUCTION YET-->\s*\r?\n?/gm, '');
-          }
-          // Use compilation.assets['index.html'].source() to avoid unnecessary string conversion
-          compilation.assets['index.html'] = {
-            source: () => html,
-            size: () => Buffer.byteLength(html, 'utf8'), // More accurate size calculation
-          };
-        }
-        callback();
-      });
-    },
+    new HtmlWorkerScriptPlugin(),
+    // function HtmlWorkerScriptPlugin() {
+    //   this.plugin('emit', (compilation, callback) => {
+    //     const htmlAsset = compilation.assets['index.html'];
+    //     if (htmlAsset) {
+    //       let html = htmlAsset.source();
+    //       // replace <script> tag for roll20 sheetworkers
+    //       html = html.replace(/<script>(.*?)<\/script>/gs, '<script type="text/worker">$1</script>');
+    //       const currentDate = new Date().toUTCString();
+    //       html = html.replace('$$CURRENTRELEASEDATE$$', currentDate);
+    //       if (mode === 'production') {
+    //         html = html.replace(/^\s*<!--DEVELOPMENT CODE\. NOT READY FOR PRODUCTION YET-->\s*\r?\n?/gm, '');
+    //       }
+    //       // Use compilation.assets['index.html'].source() to avoid unnecessary string conversion
+    //       compilation.assets['index.html'] = {
+    //         source: () => html,
+    //         size: () => Buffer.byteLength(html, 'utf8'), // More accurate size calculation
+    //       };
+    //     }
+    //     callback();
+    //   });
+    // },
   ],
   optimization: {
     minimize: true,
     minimizer: [
       new TerserPlugin({
-        minify: TerserPlugin.uglifyJsMinify,
+        extractComments: false,
         terserOptions: {
-          extractComments: false,
-          parallel: true,
-          test: /\.js(\?.*)?$/i,
+          compress: true,
+          mangle: true,
+          format: {
+            comments: false, // Removes comments
+          },
         },
       }),
     ],
